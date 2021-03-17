51e473b7ea5feaac0b2bcf1c24a21c1fd1b64433
harri <harri@yahoo.com>
2021-03-17 12:10:52 -0400

    Customized landing page(index.vue), default layout and created nav component

---
+++ client/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="bg-teal-500 text-white text-md">
+        <nav class="container flex justify-between items-center mx-auto py-5">
+            <span class="font-semibold text-2xl">Friendzy</span>
+            <div class="spaxe-x-2">
+                <template>
+                    <span>Welcome,
+                        <NuxtLink to="#" class="hover:text-teal-200">
+                            USERNAME
+                        </NuxtLink>
+                    </span>
+                        <NuxtLink to="#" class="hover:text-teal-200">
+                            FRIENDS
+                        </NuxtLink>
+                        <NuxtLink to="#" class="hover:text-teal-200">
+                            USERS
+                        </NuxtLink>
+                        <NuxtLink to="/dashboard" class="hover:text-teal-200">
+                            DASHBOARD
+                        </NuxtLink>
+                        <a href="#" class="">LOGOUT</a>
+                </template>
+                <template>
+                    <NuxtLink to="/auth/register" class="hover:text-teal-200">
+                        REGISTER
+                    </NuxtLink>
+                    <NuxtLink to="/auth/login" class="hover:text-teal-200">
+                        LOGIN
+                    </NuxtLink>
+                </template>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,78 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container max-w-6xl mx-auto mt-48 sm:px-6 lg:px-8">
+      <div class="flex justify-center items-center bg-teal-200 h-56">
+          <span class="text-gray-700 text-5xl">Friendzy</span>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
