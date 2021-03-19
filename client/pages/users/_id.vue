10b198fb7d9098466b7b852573deea7da3fdcf13
harri <harri@yahoo.com>
2021-03-19 11:16:02 -0400

    Friendship functions in User id page

--- client/pages/users/_id.vue
+++ client/pages/users/_id.vue
@@ -3,8 +3,45 @@
         <div class="flex space-x-6 mx-auto">
             <div class="flex flex-col items-end w-1/3">
                 <h2 class="text-3xl">{{ user.username }}</h2>
-                <template v-if="this.$auth.user.id != this.user.id">
-                    <form>
+
+                <template v-if="friendRequestSentFrom">
+                    <div class="flex">
+                        <form @submit.prevent="acceptFriend">
+                            <green-button type="submit">
+                                Accept
+                                <svg aria-hidden="true" data-prefix="fas" data-icon="user-plus" class="fill-current w-4 h-4 ml-1 text-white svg-inline--fa fa-user-plus fa-w-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                    <path d="M624 208h-64v-64c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v64h-64c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h64v64c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-64h64c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400 48c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/>
+                                </svg>
+                            </green-button>
+                        </form>
+                        <form @submit.prevent="denyFriend" class="ml-3">
+                            <red-button type="submit">
+                                Ignore
+                                <svg aria-hidden="true" data-prefix="fas" data-icon="user-minus" class="fill-current w-4 h-4 ml-1 text-white svg-inline--fa fa-user-minus fa-w-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                    <path d="M624 208H432c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h192c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400 48c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/>
+                                </svg>
+                            </red-button>
+                        </form>
+                    </div>
+                </template>
+
+                <template v-else-if="friendRequestSentTo">
+                    <span>Pending</span>
+                </template>
+
+                <template v-else-if="isFriendsWith">
+                    <form @submit.prevent="deleteFriend">
+                        <red-button type="submit">
+                            Unfriend
+                            <svg aria-hidden="true" data-prefix="fas" data-icon="user-minus" class="fill-current w-4 h-4 ml-1 text-white svg-inline--fa fa-user-minus fa-w-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                <path d="M624 208H432c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h192c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400 48c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/>
+                            </svg>
+                        </red-button>
+                    </form>
+                </template>
+
+                <template v-else-if="this.$auth.user.id != this.user.id">
+                    <form @submit.prevent="addFriend">
                         <blue-button type="submit">
                             Add Friend
                             <svg aria-hidden="true" data-prefix="fas" data-icon="user-plus" class="fill-current w-4 h-4 ml-1 text-white svg-inline--fa fa-user-plus fa-w-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
@@ -32,6 +69,9 @@
     export default {
         data:() => ({
             user: {},
+            isFriendsWith: '',
+            friendRequestSentTo: '',
+            friendRequestSentFrom: ''
         }),
         created() {
             this.getUser()
@@ -41,7 +81,30 @@
                 this.$axios.$get('/api/user/'+this.$route.params.id)
                     .then((resp)=>{
                         this.user = resp.user
+                        this.isFriendsWith = resp.isFriendsWith
+                        this.friendRequestSentTo = resp.friendRequestSentTo
+                        this.friendRequestSentFrom = resp.friendRequestSentFrom
                     })
+                    .catch((err)=>{console.log(err)})
+            },
+            addFriend() {
+                this.$axios.$post('/api/friends/'+this.$route.params.id)
+                    .then((resp) => this.getUser())
+                    .catch((err)=>{console.log(err)})
+            },
+            acceptFriend() {
+                this.$axios.$patch('/api/friends/'+this.$route.params.id)
+                    .then((resp) => this.getUser())
+                    .catch((err)=>{console.log(err)})
+            },
+            denyFriend() {
+                this.$axios.$get('/api/friends/'+this.$route.params.id)
+                    .then((resp) => this.getUser())
+                    .catch((err)=>{console.log(err)})
+            },
+            deleteFriend() {
+                this.$axios.$delete('/api/friends/'+this.$route.params.id)
+                    .then((resp) => this.getUser())
                     .catch((err)=>{console.log(err)})
             }
         }
